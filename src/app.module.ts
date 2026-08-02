5ac26063759a2648edf5d3f53222fb3f65002c84
Rishabh Sharma <rishabh.s2203@outlook.com>
2026-08-02 12:55:01 +0530

    feat: configure application foundation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
