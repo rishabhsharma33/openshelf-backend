ffcc5c76e7195f10d635635cc854eae79b73759c
Rishabh Sharma <rishabh.s2203@outlook.com>
2026-08-02 12:59:05 +0530

    chore: user configservice to get keys from env in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 
 bootstrap();
